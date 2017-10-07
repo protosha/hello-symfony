bb965fe4772bf8ac97b25a0e7f09cfd2167eadd9
Maxim Tsoy <protosha123@gmail.com>
2017-10-07 12:16:42 +0300

    User authentication num.2

--- src/AppBundle/Controller/AuthenticationController.php
+++ src/AppBundle/Controller/AuthenticationController.php
@@ -2,29 +2,29 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Person;
+use AppBundle\Form\LoginType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use AppBundle\Form\RegisterType;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 class AuthenticationController extends Controller
 {
     /**
-     * @Route("/sign-in")
+     * @Route("/authenticate", name="authenticate")
      */
-    public function signInAction()
+    public function authenticateAction(Request $request, AuthenticationUtils $authUtils)
     {
-        return $this->render('authentication/sign-in.html.twig', array());
-    }
-
-    /**
-     * @Route("/authenticate")
-     */
-    public function authenticateAction()
-    {
-
+        $error = $authUtils->getLastAuthenticationError();
+        $email = $authUtils->getLastUsername();
+        return $this->render('authentication/sign-in.html.twig', array(
+            'last_username' => $email,
+            'error' => $error
+        ));
     }
 }
